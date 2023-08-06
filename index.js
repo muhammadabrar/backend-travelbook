87c5959c983f064f463c748a25ad44b8d5ac6913
Hamza fAHAM <hamzafaham111@gmail.com>
2023-08-06 11:42:42 +0500

    made some changes

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
 var cors = require("cors");
+const dotenv = require('dotenv')
 // app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cors());
 // inside public directory.
 app.use("/public", express.static("public"));
 app.use(bodyParser.json());
+dotenv.config('./env');
 
 //database=============================================================
-const uri = "mongodb+srv://Asquare:Locker143@cluster0.7y43i.mongodb.net/";
-mongoose.connect(uri, {});
+const DB = process.env.DB;
+mongoose.connect(DB, {});
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
